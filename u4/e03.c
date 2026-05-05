6c36dece24d9a99425118d53b147de79c95acac7
Vittorio-Grillenzoni <vittorio.grillenzoni@galileiostiglia.edu.it>
2026-05-05 09:20:46 +0000

    provare

--- u4/e03.c
+++ u4/e03.c
@@ -4,17 +4,24 @@
 #define DIM 8
 
 int main(){
-    int numero;
-    int bin[DIM];
+    int numero;     // variabile per acquisire un valore intero da tastiera.
+    int bin[DIM];   // array per rappresentare un numero in binario.
     char junk;
 
-    // step 1: acquisisco il valore intero da tastiera
+    // step 1: acquisico il valore intero da tastiera
     printf("Inserisci un valore intero: ");
-    snaf("%d", &numero);
+    scanf("%d", &numero);
     junk = getchar();
-    
-    convertiBinario(numero, bin, DIM);
-    stampaBinario(bin, DIM);
-    printf("\n\n");
+
+    if(convertiBinario(numero, bin, DIM)){
+        //stampaBinario(bin, DIM);
+        stampaBinarioBasic(bin, DIM);
+        printf("\n\n");
+    }
+    else{
+        printf("Attenzione il numero %d non e' rappresentabile con %d bit!", numero, DIM);
+    }
     return(0);
 }
+
+// gcc e.c -lm

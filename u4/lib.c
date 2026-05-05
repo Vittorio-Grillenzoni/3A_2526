277087c8fd283e20c5a44da1b22981f45623bbb8
Vittorio-Grillenzoni <vittorio.grillenzoni@galileiostiglia.edu.it>
2026-05-05 09:18:44 +0000

    gcc

--- u4/lib.c
+++ u4/lib.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 #include "lib.h"
 
 void initVettore(int _vet[], int _dim){
@@ -12,7 +13,7 @@ void initVettore(int _vet[], int _dim){
 void stampaVettore(int _vet[], int _dim){
     int i;
     for(i=0; i<_dim; i++){
-        printf("%3d", _vet[i]);
+        printf("%3d ", _vet[i]);
     }
 }
 void stampaVettoreGrafico(int _vet[], int _dim){
@@ -47,28 +48,110 @@ void stampaVettoreGrafico(int _vet[], int _dim){
         printf("%4d", i);
     }
 }
-
-// Funzioni e03.c
-
-int convertiBinario(int _n, int _v[], int _dim){
+void stampaVettoreGraficoBasic(int _vet[], int _dim){
     int i;
-    if(_n < pow(2, _dim)){
+    // put your code here...
+    printf("|");
+    for(i=0; i<_dim; i++){
+        printf("---");
+        if(i<_dim-1)
+            printf("|");
+        else
+            printf("|");
+    }
+
+    printf("\n");
+    printf("|");
+    for(i=0; i<_dim; i++){
+        printf("%3d|", _vet[i],179);
+    }
+    printf("\n");
+    printf("|");
+    for(i=0; i<_dim; i++){
+        printf("---", 196,196,196);
+        if(i<_dim-1)
+            printf("|");
+        else
+            printf("|");
+    }
+
+    printf("\n");
+    for(i=0; i<_dim; i++){
+        printf("%4d", i);
+    }
+}
+void inputManualeArray(int _vet[], int _dim, int _min, int _max){
+    int i;
+    char junk;
+    int val;
+
+    if(_min < _max){
         for(i=0; i<_dim; i++){
-            _v[i]= _n%2;
-            _n= _n/2;
+            do{
+                printf("Inserisci valore cella [%d]: ", i);
+                scanf("%d", &val);
+                junk = getchar();
+            }while(val<_min || val>_max);
+            _vet[i] = val;
         }
-    return(1);
     }
     else{
-
+        printf("Estremi errati.");
     }
 }
 
+// --- Funzioni per e03.c ---
+int convertiBinario(int _n, int _v[], int _dim){
+    int i;
+    //if(_n<=255){  // limitata a 8 bit
+    if(_n < pow(2, _dim)){
+        for(i=0; i<_dim; i++){
+            _v[i] = _n % 2;     // salvo il resto della divisione x2 quindi il bit di posto i.
+            _n = _n / 2;        // divido il numero restante x2 per proseguire la scomposizione.
+        }
+        return(1);
+    }
+    else{
+        return(0);
+    }
+}
 void stampaBinario(int _v[], int _dim){
     int i;
-    for(i=_dim-1; i>=0; i--){
+    for(i=_dim-1; i>=0; i--){   // visualizzo il vettore dei bit binari partendo dall'ultima cella fino alla prima
         printf("%d", _v[i]);
     }
 }
+void stampaBinarioBasic(int _vet[], int _dim){
+int i;
+    // put your code here...
+    printf("|");
+    for(i=0; i<_dim; i++){
+        printf("---");
+        if(i<_dim-1)
+            printf("|");
+        else
+            printf("|");
+    }
 
-//gcc e.c -lm 
+    printf("\n");
+    printf("|");
+    for(i=0; i<_dim; i++){
+        printf("%3d|", _vet[i],179);
+    }
+    printf("\n");
+    printf("|");
+    for(i=0; i<_dim; i++){
+        printf("---", 196,196,196);
+        if(i<_dim-1)
+            printf("|");
+        else
+            printf("|");
+    }
+
+    printf("\n");
+    for(i=_dim-1; i>=0; i--){
+        printf("%4d", i);
+    }   
+}
+
+//gcc e.c -lm
--- u4/lib.h
+++ u4/lib.h
@@ -1,44 +1,83 @@
-//header contenente solamente i prototipi e relativa documentazione
-//delle funzioni di libreria
+/* header contentente solamente i prototipi e relativa documentazione
+     delle funzioni di libreria. */
+
 /**
- * @param int* Riferimento al vettore da inizializzare
- * @param int Dimensione del vettore
+ * [IT] Inizializza un vettore di interi con valori casuali da 1 a 10.
+ * 
+ * [EN] Init an array with random values between 1 an 10
+ * @param int* Array reference.
+ * @param int Array length.
  */
 void initVettore(int _vet[], int _dim);
 
 /**
- * Visualizza un vettore di valori interi
- * @param int* Riferimento al vettore da visualizzare
- * @param int Dimensione del vettore
+ * [IT] Visualizza un vettore di valori interi.
+ * 
+ * [EN] Print an array of integer values
+ * @param int* Array reference.
+ * @param int Array length.
  */
 void stampaVettore(int _vet[], int _dim);
 
 /**
- * Stampa un vettore in formato grafico, rettangolando secondo i codice della tabella
+ * [IT] Stampa un vettore in formato grafico, rettangolando secondo i codice della tabella
  * ASCII in modo da realizzare le singole celle, sotto alle celle sono visualizzati
  * gli indici del vettore.
- * @param int* Riferimento al vettore da visualizzare
- * @param int Dimensione del vettore
+ * 
+ * [EN] Print an array in a "graphical mode" where each cell has a border outside.
+ * Following the array a line with indexes is printed.
+ * @param int* Array reference.
+ * @param int Array length.
  */
 void stampaVettoreGrafico(int _vet[], int _dim);
 
-// Funzioni per e03.c
+/**
+ * UTILIZZA CARATTERI COMPATIBILI CON GITHUB CODESPACE (primi 127 caratteri della ASCII table)
+ * 
+ * [IT] Stampa un vettore in formato grafico, rettangolando secondo i codice della tabella
+ * ASCII in modo da realizzare le singole celle, sotto alle celle sono visualizzati
+ * gli indici del vettore.
+ * 
+ * [EN] Print an array in a "graphical mode" where each cell has a border outside.
+ * Following the array a line with indexes is printed.
+ * @param int* Array reference.
+ * @param int Array length.
+ */
+void stampaVettoreGraficoBasic(int _vet[], int _dim);
+/**
+ * [IT] Richiede l'inserimento manuale dei valori nel vettore con il vincolo che siano compresi tra min e max
+ * [EN] ...
+ * 
+ * @param int* Array reference.
+ * @param int Array length.
+ * @param int Min value allowed
+ * @param int Max value allowed
+ */
+void inputManualeArray(int _vet[], int _dim, int _min, int _max);
+
+// --- Funzioni per e03.c ---
 
 /**
- * funzione che converte un numero intero in un array binario.
+ * Funzione che converte un numero intero in un array binario.
  * 
- * @param int Numero da convertire
- * @param int[] riferimento al vettore "binario"
- * @param int Dimensione vettore
- * @return 0/1 in caso affermativo o negativo dell'avvenuta conversione
-*/
-int convertiBinario(int _n, int _v, int _dim);
+ * @param int Numero da convertire.
+ * @param int[] Riferimento al vettore 'binario'.
+ * @param int Dimensione del vettore.
+ * @return 0/1 in caso affermativo o negativo dell'avvenuta conversione.
+ */
+int convertiBinario(int _n, int _v[], int _dim);
 
 /**
- * Stampa  un vettore di bit 
+ * Stampa un vettore di bit in formato binario.
  * 
- * @param int[] Riferimento al vettore
- * @param int Dimensione del vettore
-*/
-
-void stampabinario(int _v[], int _dim);
+ * @param int[] Riferimento al vettore.
+ * @param int Dimensione del vettore.
+ */
+void stampaBinario(int _v[], int _dim);
+/**
+ * Stampa un vettore binario indicando la potenza di ogni singolo bit.
+ * 
+ * @param int[] Riferimento al vettore.
+ * @param int Dimensione del vettore.
+ */
+void stampaBinarioBasic(int _vet[], int _dim);
